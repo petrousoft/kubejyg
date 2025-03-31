05f4dfe5820455aa48ecfa435ee2247a8daca568
Ioannis Petrousov <contact@petrousoft.com>
2025-03-31 17:35:36 +0200

    Code cleanup (#13)

--- src/kubejyg/kubejyg.py
+++ src/kubejyg/kubejyg.py
@@ -13,16 +13,18 @@ class KubejygHelpFormatter (argparse.HelpFormatter):
     def _split_lines(self, text, width):
         return super()._split_lines(text, width) + [""]
 
+
 class KubernetesResources:
     """
     One class to rule them all.
     """
+
     def __init__(self):
         self.apps_client = client.AppsV1Api()
         self.core_client = client.CoreV1Api()
         self.networking_client = client.NetworkingV1Api()
         self.output_struct = {
-            "Namespaces" : list()
+            "Namespaces": list()
         }
         self.all_namespaces = []
 
@@ -66,7 +68,7 @@ class KubernetesResources:
 
         if len(self.output_struct["Namespaces"]) == 0:
             # If namespaces empty, initialize with current namespace
-            self.output_struct["Namespaces"].append({self.current_namespace : list()})
+            self.output_struct["Namespaces"].append({self.current_namespace: list()})
             self.current_namespace_index = -1
         else:
             namespace_found = False
@@ -75,23 +77,22 @@ class KubernetesResources:
                     namespace_found = True
                     self.current_namespace_index = i
                     break
-            if namespace_found == False:
-                self.output_struct["Namespaces"].append({self.current_namespace : list()})
+            if namespace_found is False:
+                self.output_struct["Namespaces"].append({self.current_namespace: list()})
                 self.current_namespace_index = -1
 
     # TODO: make this function generic and reduce - Pass type of resource to expand the list.
     def init_deployment_list(self):
         """ Initializes the namespace list of resources. """
-        self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace].append({"Deployments" : list()})
+        self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace].append({"Deployments": list()})
 
     def init_service_list(self):
         """ Initializes the namespaced list of resources. """
-        self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace].append({"Services" : list()})
+        self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace].append({"Services": list()})
 
     def init_ingress_list(self):
         """ Initializes the namespace list of resources. """
-        self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace].append({"Ingress" : list()})
-
+        self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace].append({"Ingress": list()})
 
     def add_deployment_to_output_struct(self, deployment):
         """ Filters necessary deployment data and appents the deployment object to the list. """
@@ -190,7 +191,6 @@ class KubernetesResources:
 
         self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace][-1]["Services"][-1].update(service_dict)
 
-
     def add_ingress_to_output_struct(self, ingress):
         """ Filters necessary ingress data and appends the ingress object to the list. """
 
@@ -211,43 +211,11 @@ class KubernetesResources:
             "uid"
         ]
 
-        spec_keys_to_remove = [
-            "paused",
-            "progress_deadline_seconds",
-            "revision_history_limit",
-        ]
-
-        spec_template_keys_to_remove = [
-            "metadata"
-        ]
-
-        # TODO: unfold
-        spec_template_spec_keys_to_remove = [
-            'active_deadline_seconds',
-            'automount_service_account_token',
-            'dns_config',
-            'dns_policy',
-            'enable_service_links', 'ephemeral_containers', 'host_aliases', 'host_ipc', 'host_network', 'host_pid', 'host_users', 'hostname', 'image_pull_secrets', 'init_containers', 'node_name', 'node_selector', 'os', 'overhead', 'preemption_policy', 'priority', 'priority_class_name', 'readiness_gates', 'resource_claims', 'restart_policy', 'runtime_class_name', 'scheduler_name', 'scheduling_gates', 'security_context', 'service_account', 'service_account_name', 'set_hostname_as_fqdn', 'share_process_namespace', 'subdomain', 'termination_grace_period_seconds', 'tolerations', 'topology_spread_constraints' 
-        ]
-
         # Remove unecessary top level metadata keys
         for k in metadata_keys_to_remove:
             ingress_dict["metadata"].pop(k)
 
-        # Remove unecessary top level spec keys
-        #for k in spec_keys_to_remove:
-        #    ingress_dict["spec"].pop(k)
-
-        # Remove duplicated metadata from spec
-        #for k in spec_template_keys_to_remove:
-        #    ingress_dict["spec"]["template"].pop(k)
-
-        # Remove redundant keys from spec->template->spec
-        #for k in spec_template_spec_keys_to_remove:
-        #    ingress_dict["spec"]["template"]["spec"].pop(k)
-
         self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace][-1]["Ingress"][-1].update(ingress_dict)
-
 
     # TODO: See if you can reduce duplication with these functions - individual resource fetching.
     def construct_all_deployments_in_all_namespaces(self):
@@ -263,12 +231,11 @@ class KubernetesResources:
                 self.init_deployment_list()
                 for each_deployment in namespaced_deployments.items:
                     deployment_header = {
-                        "apiVersion" : "apps/v1",
-                        "kind" : "Deployment" 
+                        "apiVersion": "apps/v1",
+                        "kind": "Deployment"
                     }
                     self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace][-1]["Deployments"].append(deployment_header)
                     self.add_deployment_to_output_struct(each_deployment)
-
 
     def construct_all_services_in_all_namespaces(self):
         """
@@ -283,12 +250,12 @@ class KubernetesResources:
                 self.init_service_list()
                 for each_service in namespaced_services.items:
                     service_header = {
-                        "apiVersion" : "v1",
-                        "kind" : "Service"
+                        "apiVersion": "v1",
+                        "kind": "Service"
                     }
                     self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace][-1]["Services"].append(service_header)
                     self.add_service_to_output_struct(each_service)
-            
+
     def construct_all_ingress_in_all_namespaces(self):
         """
         Adds all ingress for all namespaces into the output struct - YAML.
@@ -302,8 +269,8 @@ class KubernetesResources:
                 self.init_ingress_list()
                 for each_ingress in namespaced_ingress.items:
                     ingress_header = {
-                        "apiVersion" : "networking.k8s.io/v1",
-                        "kind" : "Ingress" 
+                        "apiVersion": "networking.k8s.io/v1",
+                        "kind": "Ingress"
                     }
                     self.output_struct["Namespaces"][self.current_namespace_index][self.current_namespace][-1]["Ingress"].append(ingress_header)
                     self.add_ingress_to_output_struct(each_ingress)
